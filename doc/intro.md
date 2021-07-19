6bf547e4230d78bb8bc85a44bb199f0a7a4beb83
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2021-07-19 10:33:50 +0200

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to work-tracker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/work_tracker/core_test.clj
@@ -0,0 +1,7 @@
+(ns work-tracker.core-test
+  (:require [clojure.test :refer :all]
+            [work-tracker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
