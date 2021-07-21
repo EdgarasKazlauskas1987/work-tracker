a82117260697f0842f273b01fed300eb86c9a41c
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2021-07-21 13:16:27 +0200

    Added main widgets for frame

--- src/work_tracker/core.clj
+++ src/work_tracker/core.clj
@@ -1,19 +1,14 @@
 (ns work-tracker.core
   (:require [seesaw.core :as seesaw]))
 
+;; ToDo make function to save work record to DB
+(defn save-work [task]
+  (println task))
 
 (defn -main [& args]
-  (defn display
-    [content]
-    (let [window (seesaw/frame :title "Example")]
-      (-> window
-          (seesaw/config! :content content)
-          (seesaw/pack!)
-          (seesaw/show!))))
-  (def button
-    (seesaw/button
-      :text "Click Me"
-      :listen [:action (fn [event](seesaw/alert "Click!" ))]))
-  (display button))
-
-
+  (let [window (seesaw/frame :title "Work Tracker" :width 300 :height 300 :on-close :hide)
+        text-field (seesaw/text :text "Add work" :editable? true :columns 20)
+        button (seesaw/button :text "Click Me" :listen [:action (fn [event](save-work (seesaw/text text-field)))])
+        panel (seesaw/horizontal-panel :items [text-field button])]
+    (seesaw/config! window :content panel)
+    (seesaw/show! window)))
