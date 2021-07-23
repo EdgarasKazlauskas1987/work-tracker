5f37a124a3f7703c394cf0c412194417689fca49
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2021-07-23 18:43:18 +0200

    Added opening work file functionality

--- src/work_tracker/core.clj
+++ src/work_tracker/core.clj
@@ -7,10 +7,11 @@
   (let [window (seesaw/frame :title (str "Work Tracker " (utils/current-time))
                              :width 900 :height 56 :resizable? false :on-close :hide)
         text-field (seesaw/text :text (first (utils/read-last-work)) :editable? true :multi-line? false)
-        button (seesaw/button :text "Click Me" :preferred-size [100 :by 30] :listen [:action (fn [event] (utils/save-work
+        save-btn (seesaw/button :text "Save" :preferred-size [100 :by 30] :listen [:action (fn [event] (utils/save-work
                                                                                                            (seesaw/text text-field)
                                                                                                            (utils/current-time)))])
-        panel (seesaw/horizontal-panel :items [text-field button])]
+        open-btn (seesaw/button :text "Open" :preferred-size [100 :by 30] :listen [:action (fn [event] (utils/open-file))])
+        panel (seesaw/horizontal-panel :items [text-field save-btn open-btn])]
     (-> window
         (seesaw/config! :content panel)
         (seesaw/show!))))
--- src/work_tracker/utils.clj
+++ src/work_tracker/utils.clj
@@ -1,11 +1,10 @@
 (ns work-tracker.utils
   (:import (java.text SimpleDateFormat)
            (java.util Date)
-           (java.io File))
+           (java.io File)
+           (java.awt Desktop))
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
-
-(def path "C:\\Users\\Edgaras\\Desktop\\WorkFolder")
 
 (defn current-time []
   (.format (SimpleDateFormat. "yyyy-MM-dd HH:mm") (new Date)))
@@ -19,33 +18,42 @@
 (defn current-day []
   (.format (SimpleDateFormat. "dd") (new Date)))
 
+(def base-path "C:\\Users\\Edgaras\\Desktop\\WorkFolder")
+
+(defn generate-paths [base-path]
+  {:year-path (str base-path "\\" (current-year))
+   :month-path (str base-path "\\" (current-year) "\\" (current-month))
+   :full-path (str base-path "\\" (current-year) "\\" (current-month) "\\" (current-day) ".txt")})
+
 (defn create-new-file []
-  (let [year-path (str path "\\" (current-year))
-        month-path (str path "\\" (current-year) "\\" (current-month))
-        full-path (str path "\\" (current-year) "\\" (current-month) "\\" (current-day) ".txt")
+  (let [{:keys [year-path month-path full-path]} (generate-paths base-path)
         f (new File full-path)]
    (when-not (.exists (io/file full-path))
      (do (.mkdir (File. year-path))
          (.mkdir (File. month-path))
          (. f createNewFile)))))
 
+(defn open-file []
+  (when (. Desktop isDesktopSupported)
+    (let [{full-path :full-path} (generate-paths base-path)
+         f (new File full-path)
+         d (. Desktop getDesktop)]
+     (. d edit f))))
+
 (defn read-work []
-  (with-open [reader (io/reader path)]
+  (with-open [reader (io/reader base-path)]
     (doall
       (csv/read-csv reader))))
 
 (defn read-last-work []
-  (let [full-path (str path "\\" (current-year) "\\" (current-month) "\\" (current-day) ".txt")]
+  (let [full-path (str base-path "\\" (current-year) "\\" (current-month) "\\" (current-day) ".txt")]
    (with-open [reader (io/reader full-path)]
     (last
       (doall
        (csv/read-csv reader))))))
 
 (defn save-work [task date]
-  (let [year (current-year)
-        month (current-month)
-        day (current-day)
-        full-path (str path "\\" year "\\" month "\\" day ".txt")]
+  (let [{full-path :full-path} (generate-paths base-path)]
     (create-new-file)
    (with-open [writer (io/writer full-path :append true)]
     (csv/write-csv writer [[task date]] :newline :cr+lf ))))
