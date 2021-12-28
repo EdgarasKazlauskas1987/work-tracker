a7c26fb17ac4c9ecde07d344458253c15d28eb49
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2021-12-28 19:00:34 +0100

    Updated buttons with bigger size and added green color to 'Save' button

--- src/work_tracker/core.clj
+++ src/work_tracker/core.clj
@@ -1,6 +1,7 @@
 (ns work-tracker.core
   (:gen-class)
-  (:import (java.time Instant Duration))
+  (:import (java.time Instant Duration)
+           (java.awt Color))
   (:require [seesaw.core :as seesaw]
             [work-tracker.utils :as utils]
             [chime.core :as chm]))
@@ -14,17 +15,17 @@
 (defn -main []
   (utils/create-new-file)
   (let [window (seesaw/frame :title (str "Work Tracker " (utils/current-time))
-                             :width 600 :height 80 :resizable? false :on-close :hide)
-        text-field (seesaw/text :text (second (utils/read-last-work)) :editable? true :multi-line? false)
-        save-btn (seesaw/button :text "Save" :preferred-size [130 :by 30] :listen [:action (fn [event] (utils/save-work
+                             :width 530 :height 95 :resizable? false :on-close :hide)
+        text-field (seesaw/text :text (second (utils/read-last-work)) :editable? true :multi-line? false :size [530 :by 35])
+        save-btn (seesaw/button :text "Save" :size [140 :by 30] :background (Color. 101,226,159) :listen [:action (fn [event] (utils/save-work
                                                                                                          (utils/current-time)
                                                                                                          (seesaw/text text-field)))])
-        open-btn (seesaw/button :text "Open" :preferred-size [130 :by 30] :listen [:action (fn [event] (if (utils/supported?)
+        open-btn (seesaw/button :text "Open" :size [140 :by 30] :listen [:action (fn [event] (if (utils/supported?)
                                                                                                          (utils/open-file)
                                                                                                          (seesaw/alert "Not supported on this OS"
                                                                                                                        :title "Error"
                                                                                                                        :type :error)))])
-        close-day-btn (seesaw/button :text "Close the Day" :preferred-size [180 :by 30] :listen [:action (fn [event] (System/exit 1))])
+        close-day-btn (seesaw/button :text "Close the Day" :size [250 :by 30] :listen [:action (fn [event] (System/exit 1))])
         top-panel (seesaw/horizontal-panel :items [text-field])
         bottom-panel (seesaw/horizontal-panel :items [save-btn open-btn close-day-btn])
         overall-panel (seesaw/vertical-panel :items [top-panel bottom-panel])]
