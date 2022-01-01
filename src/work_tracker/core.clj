bb25546df2dfeba54a1350193bf8744c8d9b0c3a
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2022-01-01 19:14:43 +0100

    Refactored code

--- src/work_tracker/core.clj
+++ src/work_tracker/core.clj
@@ -7,6 +7,7 @@
 
 (def job-id (atom nil))
 (def active-window (atom nil))
+(def default-notification-time "30")
 
 (defn setup [window panel]
   (as-> window v
@@ -16,31 +17,43 @@
 
 (defn -main []
   (utils/create-new-file)
-  (utils/create-config-file)
+  (utils/create-config-file default-notification-time)
   (let [scheduler (trapper/create-scheduler 1)
         window (seesaw/frame :title (str "Work Tracker " (utils/current-time))
                              :width 700 :height 95 :resizable? false :on-close :hide)
         msg-txt (seesaw/text :text (second (utils/read-last-work)) :editable? true :multi-line? false :size [530 :by 35])
         notification-lbl (seesaw/label :text "Remind in" :size [60 :by 35])
-        time-txt (seesaw/text :text "30" :editable? true :multi-line? false :size [50 :by 35])
-        min-lbl (seesaw/label :text "min." :size [60 :by 35])
-        save-btn (seesaw/button :text "Save" :size [140 :by 30] :background (Color. 101,226,159) :listen [:action (fn [event] (utils/save-work
-                                                                                                                                (utils/current-time)
-                                                                                                                                (seesaw/text msg-txt)))])
+        time-txt (seesaw/text :text (utils/read-time-config) :editable? true :multi-line? false :size [50 :by 35])
+        min-lbl (seesaw/label :text "minutes" :size [60 :by 35])
+        save-btn (seesaw/button :text "Save" :size [140 :by 30] :background (Color. 101, 226, 159) :listen [:action (fn [event] (utils/save-work
+                                                                                                                                  (utils/current-time)
+                                                                                                                                  (seesaw/text msg-txt)))])
         open-btn (seesaw/button :text "Open" :size [140 :by 30] :listen [:action (fn [event] (if (utils/supported?)
                                                                                                (utils/open-file)
                                                                                                (seesaw/alert "Not supported on this OS"
                                                                                                              :title "Error"
                                                                                                              :type :error)))])
         close-day-btn (seesaw/button :text "Close the Day" :size [250 :by 30] :listen [:action (fn [event] (System/exit 1))])
-        save-changes-btn (seesaw/button :text "Save changes" :size [170 :by 30] :listen [:action (fn [event] (do
-                                                                                                               (utils/save-time-config (seesaw/text time-txt))
-                                                                                                               (trapper/stop-job @job-id scheduler)
-                                                                                                               (->> (trapper/interval scheduler (utils/min-to-mls (Integer/parseInt (seesaw/text time-txt)))
-                                                                                                                                      (fn [] (seesaw/show! @active-window)) "work-tracker")
-                                                                                                                    (reset! job-id))))])
+        save-changes-btn (seesaw/button :text "Save changes" :size [170 :by 30] :listen [:action (fn [event] (let [time (seesaw/text time-txt)]
+                                                                                                               (if (and (every? #(Character/isDigit ^char %) time) (< (count time) 7))
+                                                                                                                 (do
+                                                                                                                   (utils/save-time-config (seesaw/text time-txt))
+                                                                                                                   (trapper/stop-job @job-id scheduler)
+                                                                                                                   (reset!
+                                                                                                                     job-id
+                                                                                                                     (trapper/interval
+                                                                                                                       scheduler
+                                                                                                                       (utils/min-to-mls (Integer/parseInt (utils/read-time-config)))
+                                                                                                                       (fn [] (seesaw/show! @active-window))
+                                                                                                                       "work-tracker")))
+                                                                                                                 (seesaw/alert "Please enter numbers only \n Max amount of numbers is 6" :title "Warning" :type :warning))))])
         top-panel (seesaw/horizontal-panel :items [msg-txt notification-lbl time-txt min-lbl])
         bottom-panel (seesaw/horizontal-panel :items [save-btn open-btn close-day-btn save-changes-btn])
         overall-panel (seesaw/vertical-panel :items [top-panel bottom-panel])]
-    (->> (trapper/interval scheduler 10000 (fn [] (setup window overall-panel)) "work-tracker")
-         (reset! job-id))))
+    (reset!
+      job-id
+      (trapper/interval
+        scheduler
+        (utils/min-to-mls (Integer/parseInt (utils/read-time-config)))
+        (fn [] (setup window overall-panel))
+        "work-tracker"))))
--- src/work_tracker/utils.clj
+++ src/work_tracker/utils.clj
@@ -8,8 +8,7 @@
             [clojure.java.io :as io]))
 
 (defn min-to-mls [min]
-  (-> (TimeUnit/MILLISECONDS)
-      (.convert min TimeUnit/MINUTES)))
+  (.convert (TimeUnit/MILLISECONDS) min TimeUnit/MINUTES))
 
 (defn current-time []
   (.format (SimpleDateFormat. "yyyy-MM-dd HH:mm") (new Date)))
@@ -27,52 +26,51 @@
 
 (defn generate-paths [base-path]
   {:config-path (str base-path "\\config.txt")
-   :year-path (str base-path "\\" (current-year))
-   :month-path (str base-path "\\" (current-year) "\\" (current-month))
-   :full-path (str base-path "\\" (current-year) "\\" (current-month) "\\" (current-day) ".txt")})
+   :year-path   (str base-path "\\" (current-year))
+   :month-path  (str base-path "\\" (current-year) "\\" (current-month))
+   :full-path   (str base-path "\\" (current-year) "\\" (current-month) "\\" (current-day) ".txt")})
 
 (defn create-new-file []
   (let [{:keys [year-path month-path full-path]} (generate-paths base-path)
         f (new File full-path)]
-   (when-not (.exists (io/file full-path))
-     (do (.mkdir (File. year-path))
-         (.mkdir (File. month-path))
-         (. f createNewFile)))))
-
-(defn create-config-file []
-  (let [config-path (:config-path (generate-paths base-path))
-        f (new File config-path)]
-    (when-not (.exists (io/file config-path))
-      (. f createNewFile))))
-
-(defn supported? []
-  (. Desktop isDesktopSupported))
-
-(defn open-file []
-  (let [{full-path :full-path} (generate-paths base-path)
-        f (new File full-path)
-        d (. Desktop getDesktop)]
-    (. d edit f)))
-
-(defn read-work []
-  (with-open [reader (io/reader base-path)]
-    (doall
-      (csv/read-csv reader))))
-
-(defn read-last-work []
-  (let [{full-path :full-path} (generate-paths base-path)]
-   (with-open [reader (io/reader full-path)]
-    (last
-      (doall
-       (csv/read-csv reader))))))
-
-(defn save-work [date task]
-  (let [{full-path :full-path} (generate-paths base-path)]
-    (create-new-file)
-   (with-open [writer (io/writer full-path :append true)]
-    (csv/write-csv writer [[date task]] :newline :cr+lf ))))
+    (when-not (.exists (io/file full-path))
+      (.mkdir (File. year-path))
+      (.mkdir (File. month-path))
+      (.createNewFile f))))
 
 (defn save-time-config [^String min]
   (let [config-path (:config-path (generate-paths base-path))]
     (with-open [writer (io/writer config-path)]
       (.write writer min))))
+
+(defn create-config-file [min]
+  (let [config-path (:config-path (generate-paths base-path))
+        f (new File config-path)]
+    (when-not (.exists (io/file config-path))
+      (.createNewFile f)
+      (save-time-config min))))
+
+(defn read-time-config []
+  (slurp (:config-path (generate-paths base-path))))
+
+(defn supported? []
+  (Desktop/isDesktopSupported))
+
+(defn open-file []
+  (let [{full-path :full-path} (generate-paths base-path)
+        f (new File full-path)
+        d (Desktop/getDesktop)]
+    (.edit d f)))
+
+(defn read-last-work []
+  (let [{full-path :full-path} (generate-paths base-path)]
+    (with-open [reader (io/reader full-path)]
+      (last
+        (doall
+          (csv/read-csv reader))))))
+
+(defn save-work [date task]
+  (let [{full-path :full-path} (generate-paths base-path)]
+    (create-new-file)
+    (with-open [writer (io/writer full-path :append true)]
+      (csv/write-csv writer [[date task]] :newline :cr+lf))))
